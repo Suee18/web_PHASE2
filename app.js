74d6e5db3342203cecabd8c02c75696af9d3be50
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-05-21 22:04:21 +0300

    Connected the DB, npm install mongoose, mongodb ,

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import{ mongoose} from 'mongoose';
 
 // Create Express app
 const app = express();
@@ -13,7 +14,23 @@ const __dirname = path.dirname(__filename);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from the "public" directory
+
+
+// MongoDB connection 
+const uri = 'mongodb+srv://admin1:team18@users.n2s2zdi.mongodb.net/?retryWrites=true&w=majority&appName=users';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(result => {
+    console.log('Successfully connected to MongoDB');
+    app.listen(8080, () => {
+        console.log('Server is running on http://localhost:8080');
+    });
+})
+.catch(err => {
+    console.error(' MongoDB magatsh :(', err);
+});
+
+
+//public directory
 app.use(express.static(path.join(__dirname, 'public')));
 //pages routes
 app.get('/', (req, res) => {
@@ -27,7 +44,7 @@ app.get('/registeration', (req, res) => {
 
 
 // Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
