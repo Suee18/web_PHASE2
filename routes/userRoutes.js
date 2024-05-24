93628df51cd9a486473f24a8fd610c62fb1d1fbb
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-05-25 01:43:25 +0300

    user Data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,28 +1,3 @@
-// import express from 'express';
-// import { createUser,loginUser, userProfile} from '../controllers/userController.js'; // Import controller function
-
-
-// const router = express.Router();
-
-// // Serve the registration page
-// router.get('/register', (req, res) => {
-//   res.render('pages/register', { title: 'Register' });
-// });
-// router.get('/profile', (req, res) => {
-//     res.render('pages/profile', { title: 'profile' });
-//   });
-
-// router.get('/reviews', (req, res) => {
-//     res.render('pages/reviews', { title: 'reviews' });
-//   });
-
-// // Handle the registration form submission
-// router.post('/register', createUser);
-// router.post('/login', loginUser);
-// router.get('/profile', userProfile); // Profile route
-
-
-// export default router;
 import express from 'express';
 import { createUser, loginUser, userProfile} from '../controllers/userController.js';
 
