419895bed19f9e14a008c64f9bece1d850cb44cf
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-06-14 18:42:55 +0300

    nav bar: route-> history

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,7 @@ router.post('/login', loginUser);
 // Serve the user profile page
 router.get('/profile', userProfile);
 
-//navBar logged in user routing 
+//================navBar logged in user routing =======================================================
 router.get('/user_input/create', (req, res) => {
   res.render('pages/create_plan');  // Ensure you have a corresponding EJS file for this route
 });
@@ -27,12 +27,11 @@ router.get('/user_input/history', (req, res) => {
 });
 
 
-
 router.get('/review/reviews', (req, res) => {
   res.render('pages/reviews');
 });
 
-
+//===================================================================================================
 //LOG OUT SESSION TO BE IMPLEMENTED YET
 router.get('/logout', (req, res) => {
   res.redirect('/');
