918108a1d30fa1e34c64a77857f744acc85f2e91
hanaAbdelwahab <hana.m.abdelwahab3@gmail.com>
2024-06-19 21:20:29 +0300

    statistics

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -38,3 +38,26 @@ export const getUsers = async (req, res) => {
       res.status(500).send('Internal server error');
     }
   };
+  export const getStatistics = async (req, res) => {
+    try {
+      // Fetch daily views and monthly visits as before...
+      const visits = await VisitCounter.find().sort({ date: 1 });
+      const today = new Date().setHours(0, 0, 0, 0); // Set to the beginning of today
+      const dailyViewEntry = visits.find(visit => visit.date.getTime() === today);
+      const dailyViews = dailyViewEntry ? dailyViewEntry.count : 0;
+  
+      const monthlyVisits = visits.reduce((acc, visit) => {
+        const month = visit.date.toLocaleString('default', { month: 'long', year: 'numeric' });
+        acc[month] = (acc[month] || 0) + visit.count;
+        return acc;
+      }, {});
+  
+      // Fetch total number of reviews using the static method from Review model
+      const totalReviews = await Review.countDocuments(); // Ensure to use countDocuments() or count() depending on your Mongoose version
+  
+      res.render('pages/statistics', { title: 'Statistics', dailyViews, monthlyVisits: JSON.stringify(monthlyVisits), totalReviews });
+    } catch (err) {
+      console.error('Error fetching statistics:', err);
+      res.status(500).send('Internal server error');
+    }
+  };
